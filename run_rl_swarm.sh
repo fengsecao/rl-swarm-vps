e245ca7ac9ecd54ec05dfd04827e5b8bea258ea3
EdisonHu <88478952@qq.com>
2025-09-19 13:40:33 +0800

    切换模型为0.6b

--- run_rl_swarm.sh
+++ run_rl_swarm.sh
@@ -17,7 +17,7 @@ export SWARM_CONTRACT="0xFaD7C5e93f28257429569B854151A1B8DCD404c2"
 export PRG_CONTRACT="0x51D4db531ae706a6eC732458825465058fA23a35"
 export HUGGINGFACE_ACCESS_TOKEN="None"
 export PRG_GAME=true
-export MODEL_NAME="Gensyn/Qwen2.5-0.5B-Instruct"  # 直接设置模型
+export MODEL_NAME="Qwen/Qwen3-0.6B"  # 直接设置模型
 
 # Path to an RSA private key. If this path does not exist, a new key pair will be created.
 # Remove this file if you want a new PeerID.
@@ -239,7 +239,7 @@ export HUGGINGFACE_ACCESS_TOKEN="None"
 
 # 2) 模型选择：若未通过环境变量提供，则使用默认指定模型
 if [ -z "${MODEL_NAME:-}" ]; then
-    export MODEL_NAME="Gensyn/Qwen2.5-0.5B-Instruct"
+    export MODEL_NAME="Qwen/Qwen3-0.6B"
 fi
 echo_green ">> Using model: $MODEL_NAME"
 
